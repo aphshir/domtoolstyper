6df71827a0bb54e93bee79c5a3dc973dbadaaeb6
aphshir <aphshir57@gmail.com>
2025-02-18 01:19:22 +0100

    PGP key managment

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import file_picker
+import openpgp
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  OpenpgpPlugin.register(with: registry.registrar(forPlugin: "OpenpgpPlugin"))
 }
